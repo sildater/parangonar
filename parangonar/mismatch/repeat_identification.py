ca2c569005de18d5f146e50f29c7f4bc8f8dcbec
sildater <silvandavidpeter@gmail.com>
2025-05-14 19:32:43 +0200

    repeat identifier

--- parangonar/mismatch/repeat_identification.py
+++ parangonar/mismatch/repeat_identification.py
@@ -3,3 +3,35 @@
 """
 This module contain methods for (repeat) structure identification
 """
+
+
+class RepeatIdentifier(object):
+    """
+    method wrapper to compute the most likely and musically sensible
+    (starts at the start, ends at the end, repeats a valid number of times) 
+    sequence of score sections that correspond to an input performance.
+
+
+    Parameters
+    ----------
+
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self, score, performance):
+        """
+        Parameters
+        ----------
+        score: Score
+            a partitura Score object
+        performance : Performance
+            a partitura Performance object
+
+        Returns
+        -------
+
+        """
+      
+        return None
