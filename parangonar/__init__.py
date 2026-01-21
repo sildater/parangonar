ae9c58fc1090cb232090a749bf308fe44b261453
sildater <silvandavidpeter@gmail.com>
2026-01-21 12:18:52 +0100

    remove setup.py, pkg_resources

--- parangonar/__init__.py
+++ parangonar/__init__.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-The top level of the package contains functions to
-note-align symbolic music data.
+The top level of the package 
+contains functions to
+align music data.
 """
-import pkg_resources
 
-EXAMPLE = pkg_resources.resource_filename("parangonar", "assets/mozart_k265_var1.match")
-ALIGNMENT_TRANSFORMER_CHECKPOINT = pkg_resources.resource_filename(
-    "parangonar", "assets/alignment_transformer_checkpoint.pt"
-)
-THEGLUENOTE_CHECKPOINT = pkg_resources.resource_filename(
-    "parangonar", "assets/thegluenote_small_checkpoint.pt"
-)
+import sys
+# Use importlib.metadata and importlib.resources for modern Python versions
+if sys.version_info >= (3, 9):
+    from importlib.metadata import version
+    from importlib.resources import files
+else:
+    # Backport for Python 3.7-3.8
+    try:
+        from importlib_metadata import version
+    except ImportError:
+        from importlib.metadata import version
+    try:
+        from importlib_resources import files
+    except ImportError:
+        from importlib.resources import files
+
+# define a version variable
+__version__ = version("parangonar")
+
+#: An example MusicXML file for didactic purposes
+EXAMPLE = str(files("parangonar") / "assets" / "mozart_k265_var1.match")
+ALIGNMENT_TRANSFORMER_CHECKPOINT = str(files("parangonar") / "assets" / "alignment_transformer_checkpoint.pt")
+THEGLUENOTE_CHECKPOINT = str(files("parangonar") / "assets" / "thegluenote_small_checkpoint.pt")
+
 from .match import (
     AnchorPointNoteMatcher,
     AutomaticNoteMatcher,
